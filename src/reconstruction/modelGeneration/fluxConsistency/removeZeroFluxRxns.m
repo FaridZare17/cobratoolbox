a602569861a0d6cbac7a8abc5fefa2c7a3707e29
Farid Zare <50587630+FaridZare17@users.noreply.github.com>
2019-09-23 16:06:38 +0330

    update removeZeroFluxRxns

---
+++ src/reconstruction/modelGeneration/fluxConsistency/removeZeroFluxRxns.m
@@ -0,0 +1,46 @@
+function modelOut = removeZeroFluxRxns(model)
+% Removes the reactions that cannot have any fluxes .
+% This function sets each reaction as the objective function and applies
+% FBA to find maximum and minuimum flux that can be carried through that
+% reaction, and removes the reactions that cannot have any fluxes.
+% notice: the results have functional dependency, not structural. results
+% may change due to a change in substrate for a model.
+%
+% USAGE:
+%    new_Model = removeZeroFluxRxns(model, reactions)
+%
+% INPUTS:
+%    model:                      COBRA model structure (must define `.c`, `.lb`, `.ub`, `.S`, `.rxns`, `.mets`)
+%
+%OPTIONAL INPUT
+%    reactions:                Cell array of reaction names in the model
+%
+% OUTPUT:
+%    new_model:             Curated model, the model with removed reactions
+%                                     incapable of having a flux value, and corrected
+%                                     upper and lower bounds.
+%
+% .. Author: - Farid Zare  5/31/2019
+%...Further modifications suggested by: -Thomas Pfau 6/1/2019
+
+checkModel = verifyModel(model,'FBAOnly',true,'simpleCheck', true);
+
+if ~checkModel
+    error('The input model is invalid. Please check the model with ''verifyModel(model)'' and correct all indicated errors');
+end
+
+modelOut = model;
+[minFlux, maxFlux] = fluxVariability(modelOut, 0, 'max');
+
+%Set bounds to their feasible values in the network
+modelOut.lb = minFlux;
+modelOut.ub = maxFlux;
+
+%Difining a threshold
+feasTol = getCobraSolverParams('LP','feasTol');
+index = abs(modelOut.ub) < feasTol & abs(modelOut.lb) < feasTol;
+
+%Remove reactions with fluxes in range of threshold
+modelOut = removeRxns(modelOut, model.rxns(index));
+
+end
